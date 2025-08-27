b9d44042dcabe502c559459009b41e37110e520e
CarlosSalasTec2 <L25550691@chihuahua2.tecnm.mx>
2025-08-27 16:40:56 -0600

    Add files via upload

---
+++ EVA1_3_DATOS/src/eva1_3_datos/EVA1_3_DATOS.java
@@ -0,0 +1,19 @@
+
+package eva1_3_datos;
+
+/**
+ *
+ * @author anago
+ */
+public class EVA1_3_DATOS {
+
+    public static void main(String[] args) {
+     System.out.print("NOMBRE DEL ESTUDIANTE:");
+     System.out.println("CARLOS ALEXANDER");
+     System.out.print("NUMERO DE CONTROL:");
+     System.out.println(25550691);
+     System.out.print("EDAD:");
+     System.out.println(17);
+    }
+    
+}
