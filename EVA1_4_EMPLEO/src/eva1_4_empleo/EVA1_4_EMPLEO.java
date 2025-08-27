3594c73182fc1228ed5358c5d14437bc79e30935
CarlosSalasTec2 <L25550691@chihuahua2.tecnm.mx>
2025-08-27 16:53:01 -0600

    Add files via upload

---
+++ EVA1_4_EMPLEO/src/eva1_4_empleo/EVA1_4_EMPLEO.java
@@ -0,0 +1,19 @@
+
+package eva1_4_empleo;
+
+
+public class EVA1_4_EMPLEO {
+
+    
+    public static void main(String[] args) {
+   System.out.print("EMPLEO:");
+   System.out.println("DESAROLLADOR");
+   System.out.print("SALARIO:");
+   System.out.println("12000 PESOS");
+   System.out.print("HORAS SEMANA:");
+   System.out.println("8 HORAS");
+   System.out.print("REQUIERE INGLES?");
+   System.out.println("SI");
+    }
+    
+}
