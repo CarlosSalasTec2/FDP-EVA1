b0fa5267c07c582c249930cba365f7cac99c665f
CarlosSalasTec2 <L25550691@chihuahua2.tecnm.mx>
2025-08-28 16:54:52 -0600

    Add files via upload

---
+++ EVA1_5_VARIABLES/src/eva1_5_variables/EVA1_5_VARIABLES.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_5_variables;
+
+
+public class EVA1_5_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //DECLARAR VARIABLES
+        //IDENTIFICADOR=VALOR
+       String nombre;
+       int edad;
+       double salario;
+       //INICIALIZACION
+       //=-->OPERADOR DE ASIGNACION
+       nombre="CARLOS ALEXANDER SALAS GOMEZ";
+       edad=17;
+       salario=1.1;
+       System.out.println(nombre);
+       System.out.println(edad);
+       System.out.println(salario);
+    }
+    
+}
