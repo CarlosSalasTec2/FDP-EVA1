ecf6e3735afea087376fe16e15c6362afa69bb7a
CarlosSalasTec2 <L25550691@chihuahua2.tecnm.mx>
2025-09-01 16:43:26 -0600

    Add files via upload

---
+++ EVA1_6_OPERACIONES/src/eva1_6_operaciones/EVA1_6_OPERACIONES.java
@@ -0,0 +1,22 @@
+
+package eva1_6_operaciones;
+
+
+public class EVA1_6_OPERACIONES {
+
+    
+    public static void main(String[] args) {
+    double area,p,radio;
+//igua a    
+    //double area;
+    //double p;
+    //double radio;
+    
+    p=3.1416;
+    radio=10;
+    area=p*(radio*radio);
+    System.out.println("el area del ciurculo del radio=10 es:");
+    System.out.println(area);
+    }
+    
+}
---
+++ EVA1_7_AREA_TRIANGULO/src/eva1_7_area_triangulo/EVA1_7_AREA_TRIANGULO.java
@@ -0,0 +1,18 @@
+
+package eva1_7_area_triangulo;
+
+
+public class EVA1_7_AREA_TRIANGULO {
+
+    
+    public static void main(String[] args) {
+         double area,base,altura;
+         
+         base=20;
+         altura=10;
+         area=(base*altura)/2;
+         System.out.println("el area de un triangulo de base=20 y altura=10 es:");
+         System.out.println(area);
+    }
+    
+}
