1df28a21a60d5a1ea38455412a2e46d0fc81dc7c
CarlosSalasTec2 <L25550691@chihuahua2.tecnm.mx>
2025-09-02 16:16:46 -0600

    Add files via upload

---
+++ EVA1_8_GRADOS/src/eva1_8_grados/EVA1_8_GRADOS.java
@@ -0,0 +1,17 @@
+
+package eva1_8_grados;
+
+
+public class EVA1_8_GRADOS {
+
+    
+    public static void main(String[] args) {
+        double f,c;
+        
+        f=40;
+        c=(f-32)/1.8;
+        System.out.println("El equivalente de 40 grados farenheit en celsius es:");
+        System.out.println(c);
+    }
+    
+}
